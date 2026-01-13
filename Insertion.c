a50078052475b12d4ba63c33f498af31586c4980
Md Farhatus Sium <252-35-001@diu.edu.bd>
2026-01-13 19:36:21 +0600

    Add implementation for inserting an element into an array

---
+++ Insertion.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+int n,pos,val;
+int arr[n];
+printf("Enter the number of elements in the array: ");
+scanf("%d",&n);
+printf("Enter the elements of the array:\n");
+for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+}
+printf("Enter the position to insert the new element (0 to %d): ", n);
+scanf("%d",&pos);
+if(pos<0 || pos>n){
+    printf("Invalid position!\n");
+    return 1;
+}
+printf("Enter the value to insert: ");
+scanf("%d",&val);
+for(int i=n;i>pos;i--){
+    arr[i]=arr[i-1];
+}
+arr[pos]=val;
+printf("Element inserted successfully!\n");
+return 0;
+}
