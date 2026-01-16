08c9ee6f74447dad43a562238b6fecf0539c58ce
Md Farhatus Sium <252-35-001@diu.edu.bd>
2026-01-16 22:42:23 +0600

    Add implementation of bubble sort algorithm in C

---
+++ BubbleSort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void BubbleSort(int a[], int n){
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            if(a[j] > a[j + 1]){
+                swap(&a[j], &a[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int a[] = {5, 1, 3, 4, 2};
+    int n = sizeof(a) / sizeof(a[0]);
+
+    BubbleSort(a, n);
+
+    printf("Sorted Array: ");
+    for(int i = 0; i < n; i++){
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
